b435f3694c42c4a70ce06215b48fa6ff9f34cb32
Luke Wood <lukewoodcs@gmail.com>
2017-08-23 09:47:47 -0500

    eslint config

--- swimmingfish/.eslintrc.js
+++ swimmingfish/.eslintrc.js
@@ -1,13 +1,40 @@
 module.exports = {
-  "plugins": [
-    "react"
-  ],
-  "parserOptions": {
-    "ecmaVersion": 6,
-    "sourceType": "module",
-    "ecmaFeatures": {
-      "jsx": true,
-      "modules": true
-    }
+  'env': {
+      'browser': true,
+      'es6': true
+  },
+  'extends': 'eslint:recommended',
+  'parserOptions': {
+      'ecmaFeatures': {
+          'jsx': true
+      },
+      'sourceType': 'module'
+  },
+  'plugins': ['react'],
+  'rules': {
+      'indent': [2, 4],
+      'linebreak-style': [2, 'unix'],
+      'quotes': [2, 'single'],
+      'semi': [2, 'never'],
+      'brace-style': [2, '1tbs'],
+      'array-bracket-spacing': [2, 'never'],
+      'camelcase': [2, {'properties': 'always'}],
+      'keyword-spacing': [2],
+      'eol-last': [2],
+      'no-trailing-spaces': [2]
+  },
+  'globals': {
+      // Collections
+      'Persons': true,
+      'Modules': true,
+
+      // More stuff
+      // [...]
+
+      // Packages
+      'lodash': true,
+      'i18n': true,
+      'moment': true,
+      'Messenger': true
   }
 }

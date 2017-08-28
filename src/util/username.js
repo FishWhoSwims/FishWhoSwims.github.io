c88fd0d0062fa19aa22634b60f1817dc4f7d26f8
Luke Wood <lukewoodcs@gmail.com>
2017-08-28 09:26:11 -0500

    added in support for window.localstorage

--- src/util/username.js
+++ src/util/username.js
@@ -1,6 +1,9 @@
-let username = null;
+let username = window.localStorage.getItem('username');
+username = username || null;
+
 function setUserName(name) {
   username = name;
+  window.localStorage.setItem('username', username);
 }
 function getUserName() {
   return username;

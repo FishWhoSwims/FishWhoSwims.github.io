2b0ecc5696b4d21832b05fc1d99139558ff6ab15
Yichen Duan <vilnatar@gmail.com>
2017-08-28 12:02:36 -0500

    little change

--- src/Assignments/DetailPage.js
+++ src/Assignments/DetailPage.js
@@ -1,6 +1,16 @@
 import React, {Component} from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
+import FlatButton from 'material-ui/FlatButton';
 
+const cardInfo = {    
+  assignName: 'Exploring Table Data',
+  createDate: '2017-8-27 8:00',
+  dueDate: '2017-9-1 23:59',
+  ptWorth: 10,
+  finished: true,
+  pastDue: false
+};
 
 
 
@@ -9,13 +19,31 @@ class DetailPage extends Component{
   constructor(){
     super();
     this.state = {
-      assignmentInfo: []
+      assignmentInfo: cardInfo,
     };
   }
+
+  
 
   render(){
     return (
       (<div>
+        <MuiThemeProvider>
+          <Card>
+            <CardMedia
+              overlay={<CardTitle title="Overlay title" subtitle="Overlay subtitle" />}
+            >
+            </CardMedia>
+            <CardTitle title={this.state.assignmentInfo.assignName} subtitle="Card subtitle" />
+            <CardText>
+              {"DUE BY " + this.state.assignmentInfo.dueDate}
+            </CardText>
+            <CardActions>
+              <FlatButton label="EDIT" />
+              <FlatButton label="RETURN" />
+            </CardActions>
+          </Card>
+        </MuiThemeProvider>
       </div>)
     );
   }

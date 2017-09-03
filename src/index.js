4c8e064ab9a302533df8d836c4cdb91ac096773c
Nhi Dang <dangnhi169@gmail.com>
2017-09-03 00:18:21 -0500

    adding background image

--- src/index.js
+++ src/index.js
@@ -3,13 +3,6 @@ import ReactDOM from 'react-dom';
 import style from './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-//import Background from './background.jpg'
-
-// const styles = {
-//   root: {
-//     backgroundImage: "url('./background.jpg')"
-//   }
-// }
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();

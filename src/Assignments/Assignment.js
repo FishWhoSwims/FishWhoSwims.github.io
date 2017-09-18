82f376b61c345a5b7b47787cc3c95b8badf5613a
Elena Sharp <eesharp@smu.edu>
2017-09-18 12:26:01 -0500

    Added Delete and minor redirect changes

--- src/Assignments/Assignment.js
+++ src/Assignments/Assignment.js
@@ -1,11 +1,22 @@
 import React, {Component} from 'react';
 import { Redirect } from 'react-router';
+import IconButton from 'material-ui/IconButton';
+import DeleteIcon from 'material-ui/svg-icons/action/delete';
 import { getMaterialID, setMaterialID } from '../util/materialInfo.js';
+import DeleteModal from './DeleteModal';
+import { getUsername, setUsername } from '../util/username.js';
+import { getCourseID, setCourseID } from '../util/courseInfo.js';
+import targetUrl from '../util/targetUrl.js';
 
 import {
   TableRowColumn,
   TableRow,
 } from 'material-ui/Table';
+
+let modalStyle = {
+  'top': '50%',
+  'left': '50%',
+};
 
 class Assignment extends Component{
 
@@ -13,21 +24,73 @@ class Assignment extends Component{
     super();
     this.state = {
       redirect: null,
+      showDeleteForm: false,
+      userID : getUsername(),
+      courseID : getCourseID()
     };
+
+    this.closeDeleteModal = this.closeDeleteModal.bind(this);
+    this.addDelete = this.addDelete.bind(this);
+    this.deleteMaterial = this.deleteMaterial.bind(this);
   }
 
   goToSpecifics() {
-    if(this.props.type === 'assignment'){
-      this.setState({ redirect: '/detailpage/assignmentdetail' });
-    }
-    else if (this.props.type === 'note'){
-      this.setState({ redirect: '/detailpage/notedetail' });
-    }
-    else if (this.props.type === 'exam') {
-      this.setState({ redirect: '/detailpage/examdetail' });
-    }
-    setMaterialID(this.props.data.courseMaterialID);
+      if(this.props.type === 'assignment'){
+        this.setState({ redirect: '/detailpage/assignmentdetail' });
+        setMaterialID(this.props.data.courseMaterialID);
+      }
+      else if (this.props.type === 'note'){
+        this.setState({ redirect: '/detailpage/notedetail' });
+        setMaterialID(this.props.data.courseMaterialID);
+      }
+      else if (this.props.type === 'exam') {
+        this.setState({ redirect: '/detailpage/examdetail' });
+        setMaterialID(this.props.data.examID);
+      }
   };
+
+
+  deleteMaterial(data){
+    if (this.props.type === 'assignment') {
+      fetch(targetUrl + '/users/' + this.state.userID + '/classes/' + this.state.courseID + '/assignments/' + this.props.data.courseMaterialID + '/', {
+        method: "delete",
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+      }).then((response) => {
+
+      });
+    }
+    else if (this.props.type === 'note') {
+      fetch(targetUrl + '/users/' + this.state.userID + '/classes/' + this.state.courseID + '/notes/' + this.props.data.courseMaterialID + '/', {
+        method: "delete",
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+      }).then((response) => {
+
+      });
+    }
+
+    this.forceUpdate();
+    
+  }
+
+  addDelete(e) {
+    e.stopPropagation();
+    this.setState({
+      showDeleteForm: true,
+    });
+  }
+
+  closeDeleteModal() {
+    this.setState({
+      showDeleteForm: false
+    });
+  }
+
   render(){
     if (this.state.redirect != null) {
       return <Redirect to={`${this.state.redirect}`} />;
@@ -41,6 +104,19 @@ class Assignment extends Component{
           <TableRowColumn>{data.name}</TableRowColumn>
           <TableRowColumn>{data.date}</TableRowColumn>
           <TableRowColumn>{name ? name : 'Not Assigned'}</TableRowColumn>
+          <TableRowColumn>
+            <IconButton onClick={this.addDelete}>
+              <DeleteIcon />{
+                this.state.showDeleteForm
+                  ? <DeleteModal
+                    style={modalStyle}
+                    closeFormModal={this.closeDeleteModal}
+                    sendData={this.deleteMaterial}
+                    parentState={this.props} />
+                  : null
+              }
+            </IconButton>
+          </TableRowColumn>
         </TableRow>
       );
     }
@@ -52,6 +128,7 @@ class Assignment extends Component{
           <TableRowColumn>{data.name}</TableRowColumn>
           <TableRowColumn>{data.date}</TableRowColumn>
           <TableRowColumn>------</TableRowColumn>
+          <TableRowColumn></TableRowColumn>
         </TableRow>
       );
     }
